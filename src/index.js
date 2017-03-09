5437aeaf51b3a26c54694c5d2afcd20f833b4ac5
takahiro-saeki <hiroooon69@gmail.com>
2017-03-09 16:12:29 +0900

    update basic update.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import {generateSimple, separate, adjust} from './js/init';
 import data from './js/data.js';
 
-const generate = len => {
+export const generate = len => {
   let init = generateSimple(separate(data), len);
   let ren = adjust(init);
   return ren;
 }
-
-export default generate;
